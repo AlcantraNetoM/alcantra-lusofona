51f493e96aeed2f842d1910a666773135cca0ccc
Alcantra Neto Manuel <126702437+AlcantraNetoM@users.noreply.github.com>
2025-10-17 19:24:10 +0000

    act

---
+++ Lab 6/main.js
@@ -0,0 +1,102 @@
+// Inicializar localStorage se não existir
+if (!localStorage.getItem('produtos-selecionados')) {
+    localStorage.setItem('produtos-selecionados', JSON.stringify([]));
+}
+
+let cesto = JSON.parse(localStorage.getItem('produtos-selecionados'));
+
+// Espera que o DOM esteja carregado
+document.addEventListener('DOMContentLoaded', () => {
+    carregarProdutos(produtos);
+    atualizarCesto();
+});
+
+// Carregar produtos da loja
+function carregarProdutos(produtos) {
+    const listaProdutos = document.getElementById('lista-produtos');
+
+    produtos.forEach(produto => {
+        const artigo = criarProduto(produto);
+        listaProdutos.append(artigo);
+    });
+}
+
+// Criar produto da loja
+function criarProduto(produto) {
+    const artigo = document.createElement('article');
+    artigo.classList.add('produto-item');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = produto.title;
+    artigo.append(h3);
+
+    const img = document.createElement('img');
+    img.src = produto.image;
+    img.alt = produto.title;
+    artigo.append(img);
+
+    const descricao = document.createElement('p');
+    descricao.textContent = produto.description;
+    artigo.append(descricao);
+
+    const preco = document.createElement('p');
+    preco.textContent = `Preço: € ${produto.price.toFixed(2)}`;
+    artigo.append(preco);
+
+    const btn = document.createElement('button');
+    btn.textContent = '+ Adicionar ao Cesto';
+    artigo.append(btn);
+
+    btn.addEventListener('click', () => {
+        cesto.push(produto);
+        localStorage.setItem('produtos-selecionados', JSON.stringify(cesto));
+        atualizarCesto();
+    });
+
+    return artigo;
+}
+
+// Atualizar Cesto
+function atualizarCesto() {
+    const cestoDiv = document.getElementById('lista-cesto');
+    const totalSpan = document.getElementById('total-cesto');
+    cesto = JSON.parse(localStorage.getItem('produtos-selecionados')) || [];
+    cestoDiv.innerHTML = '';
+
+    let total = 0;
+
+    cesto.forEach((produto, index) => {
+        total += produto.price;
+        const artigo = criaProdutoCesto(produto, index);
+        cestoDiv.append(artigo);
+    });
+
+    totalSpan.textContent = total.toFixed(2);
+    document.getElementById('contador-cesto').textContent = cesto.length;
+}
+
+// Criar produto no Cesto
+function criaProdutoCesto(produto, index) {
+    const artigo = document.createElement('article');
+    artigo.classList.add('cesto-item');
+
+    const h3 = document.createElement('h3');
+    h3.textContent = produto.title;
+    artigo.append(h3);
+
+    const preco = document.createElement('p');
+    preco.textContent = `Preço: € ${produto.price.toFixed(2)}`;
+    artigo.append(preco);
+
+    const btnRemover = document.createElement('button');
+    btnRemover.textContent = 'Remover';
+    artigo.append(btnRemover);
+
+    btnRemover.addEventListener('click', () => {
+        cesto.splice(index, 1);
+        localStorage.setItem('produtos-selecionados', JSON.stringify(cesto));
+        atualizarCesto();
+    });
+
+    return artigo;
+}
---
+++ Lab 6/produtos.js
@@ -0,0 +1,30 @@
+const produtos = [
+  {
+    "id": 1,
+    "title": "O mundo do binário",
+    "price": 22.5,
+    "description": "Mostra ao mundo que percebes de matemática fora da caixa! Com esta t-shirt, podes provar que 1 + 1 = 10... mas só para os génios da programação e entusiastas do sistema binário.",
+    "category": "T-shirts",
+    "image": "https://deisishop.pythonanywhere.com/media/produto_imagens/tshirt-1-1.png",
+    "rating": {"rate": 4.7,"count": 200}
+  },
+  {
+    "id": 10,
+    "title": "As 6 fases do debugging",
+    "price": 13,
+    "description": "A jornada épica de todo programador, onde cada erro é uma montanha a escalar.",
+    "category": "Canecas",
+    "image": "https://deisishop.pythonanywhere.com/media/produto_imagens/mug-debugging.png",
+    "rating": {"rate": 3.8,"count": 230}
+  },
+  {
+    "id": 7,
+    "title": "Hello World",
+    "price": 7.5,
+    "description": "Para quem vive entre o código e o estilo! Estas meias são a combinação perfeita de geek e confortável.",
+    "category": "Meias",
+    "image": "https://deisishop.pythonanywhere.com/media/produto_imagens/socks-00.png",
+    "rating": {"rate": 4.2,"count": 125}
+  }
+  // Pode adicionar os outros produtos da mesma forma
+];
